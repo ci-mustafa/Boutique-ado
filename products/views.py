7619943f9410eab3a5abf6e259313214def8da8e
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-11-24 13:11:14 +0000

    feat: add category filltering

--- products/views.py
+++ products/views.py
@@ -9,7 +9,12 @@ def all_products(request):
     """ A view to list all products including sorting and search quries """
     products = models.Product.objects.all()
     query = None
+    categories = None
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = models.Category.objects.filter(name__in=categories)
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,7 +26,8 @@ def all_products(request):
 
     context = {
         'products': products,
-        'search_term': query
+        'search_term': query,
+        'current_categories': categories,
     }
     return render(request, 'products/products.html', context=context)
 
