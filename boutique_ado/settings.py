cc9da73806e45cd9c99525a5d61da1fde3e9d5ed
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-12-09 16:53:52 +0000

    feat: add checkout view and template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 
 ]
 
@@ -68,6 +69,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
+CIRSPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                # 'crispy_forms.templatetags.crispy_forms.field',
+            ]
         },
     },
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    """
+    Handle the checkout process, including validating the shopping bag
+    and rendering the checkout page with an order form.
+    """
+    # Retrieve the shopping bag from the session
+    bag = request.session.get('bag', {})
+    
+    # Check if the bag is empty
+    if not bag:
+        # Display an error message to the user if the bag is empty
+        messages.error(request, "There is nothing in your bag at the moment!")
+        # Redirect the user to the 'all-products' page if the bag is empty
+        return redirect(reverse('all-products'))
+    
+    # Create a blank instance of the order form
+    order_form = OrderForm()
+    
+    # Specify the template to render
+    template = "checkout/checkout.html"
+    
+    # Context data to pass to the template
+    context = {
+        'order_form': order_form  # Include the order form in the context
+    }
+    
+    # Render the checkout template with the provided context
+    return render(request, template, context)
+
+    
