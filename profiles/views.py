c38143f1ff5ac448355ede86dfb395d0908ff397
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-12-22 11:54:19 +0000

    feat: make secure prodcut and profile endpoints

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
 
-
+@login_required
 def profile(request):
     """
     Display and handle updates to the user's profile.
