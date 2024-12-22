0edf74431b9e4422d8cb2f7bbf75997999cb55cd
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-12-22 11:37:25 +0000

    feat: add delete functionality on prodcuts

--- products/views.py
+++ products/views.py
@@ -71,9 +71,9 @@ def add_product(request):
     if request.method == 'POST':
         form = forms.ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product-detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -109,4 +109,12 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(models.Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('all-products'))
 
