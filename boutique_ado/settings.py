2a68297513a3bc12c9a43aa8f81b17e1fa93fa2a
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-12-23 08:34:35 +0000

    feat: deployment, create procfile, database url, modify settings prapare for deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,10 +33,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-cimustafa-boutiqueado-jvl1sa163bm.ws-eu117.gitpod.io',
+    ".herokuapp.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-cimustafa-boutiqueado-jvl1sa163bm.ws-eu117.gitpod.io',
+    'https://*.herokuapp.com',
 ]
 
 
@@ -135,12 +138,17 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
